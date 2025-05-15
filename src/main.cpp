b0db1aa2bc0cfefbb026f1368fd1eacbbc431a38
KarolBetlejewski <s203232@student.pg.edu.pl>
2025-05-15 10:48:11 +0200

    Dodanie funkcji sinusoidalnej i cosinusoidalnej, mozliwosc wyboru czestotliwosci (narazie z punktu int main(){} )

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,76 @@
 #include "../matplotplusplus/source/matplot/matplot.h"
+#include <cmath>
+#include <vector>
+#include <iostream>
 
-int dodawanko(int a, int b){
-    return a + b;
-}
+using namespace std;
 
-int main() {
+int Sinus(double freq) {
     using namespace matplot;
 
-    // przykład wykresu
-    plot({1, 3, 2, 4, 5});
-    title("Mój pierwszy wykres z matplot++");
-    xlabel("oś X");
-    ylabel("oś Y");
+    // Zakres czasu (np. 0 do 1 sekundy)
+    std::vector<double> x;
+    std::vector<double> y;
+
+    double sampling_rate = 1000; // 1000 próbek na sekundę
+    double duration = 1.0;       // czas trwania sygnału w sekundach
+
+    for (int i = 0; i < duration * sampling_rate; ++i) {
+        double t = i / sampling_rate;
+        x.push_back(t);
+        y.push_back(std::sin(2 * pi * freq * t));
+    }
+
+    // Rysowanie wykresu
+    plot(x, y);
+    title("Wykres funkcji sinusoidalnej");
+    xlabel("Czas [s]");
+    ylabel("Amplituda");
+    grid(on);
+
     show();
+
+    return 0;
+}
+
+int Cosinus(double freq) {
+    using namespace matplot;
+
+    // Zakres czasu (np. 0 do 1 sekundy)
+    std::vector<double> x;
+    std::vector<double> y;
+
+    double sampling_rate = 1000; // 1000 próbek na sekundę
+    double duration = 1.0;       // czas trwania sygnału w sekundach
+
+    for (int i = 0; i < duration * sampling_rate; ++i) {
+        double t = i / sampling_rate;
+        x.push_back(t);
+        y.push_back(std::cos(2 * pi * freq * t));
+    }
+
+    // Rysowanie wykresu
+    plot(x, y);
+    title("Wykres funkcji cosinusoidalnej");
+    xlabel("Czas [s]");
+    ylabel("Amplituda");
+    grid(on);
+
+    show();
+
+    return 0;
+}
+
+//narazie dalem int main aby testowac funkcje
+int main() {
+
+    double freq_choice;
+
+    std::cout << "Podaj częstotliwość (Hz): ";
+    std::cin >> freq_choice;
+    
+    //Cosinus(freq_choice);
+    //Sinus(freq_choice);
 
     return 0;
 }
